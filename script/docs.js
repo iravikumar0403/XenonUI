9d68b5c62b80a27d7a273f23ac35804dfb8a91d1
Ravi Kumar <ravikumr0403@gmail.com>
2022-02-05 09:22:29 +0530

    fix: navigation

--- script/docs.js
+++ script/docs.js
@@ -1,34 +1,41 @@
-window.addEventListener('hashchange', (e)=>{
-    const currentHash = e.newURL.split("#")[1]
-    setActivePage(currentHash)
-})
+window.addEventListener("hashchange", (e) => {
+  const currentHash = e.newURL.split("#")[1];
+  setActivePage(currentHash);
+});
 
 window.onload = () => {
-    if(window.location.hash){
-        currentHash = window.location.hash.slice(1)
-        setActivePage(currentHash)
-    }else{
-        setActivePage("avatar")
-    }
+  if (window.location.hash) {
+    currentHash = window.location.hash.slice(1);
+    setActivePage(currentHash);
+  } else {
+    setActivePage("avatar");
+  }
+};
+
+function setActivePage(currentHash) {
+  window.scrollTo(0, 0);
+  if (currentHash === "") {
+    history.go(-1);
+    return;
+  }
+  const prevActive = document.querySelector(".sidebar-menu-item.active");
+  prevActive && prevActive.classList.remove("active");
+  const divs = document.querySelectorAll("main > div");
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].style.display = "none";
+  }
+
+  const currentActive = document.querySelector(`a[href="#${currentHash}"]`);
+  document.getElementsByClassName("sidebar")[0].classList.remove("show");
+  menuBtn.children[0].classList.remove("fa-times");
+  currentActive.parentElement.classList.add("active");
+  document.getElementById(currentHash).style.display = "block";
 }
 
-function setActivePage(currentHash){
-    window.scrollTo(0,0)
-    const prevActive = document.querySelector(".sidebar-menu-item.active")
-    prevActive && prevActive.classList.remove('active')
-    const divs = document.querySelectorAll("main > div")
-    for(let i=0;i<divs.length;i++){
-        divs[i].style.display = "none"
-    }
+const menuBtn = document.querySelector(".nav-menu-resp");
 
-    const currentActive = document.querySelector(`a[href="#${currentHash}"]`)
-    currentActive.parentElement.classList.add('active')
-    document.getElementById(currentHash).style.display = "block"
-}
-
-const menuBtn = document.querySelector(".nav-menu-resp")
-menuBtn.addEventListener('click', ()=>{
-    const sideNav = document.querySelector(".sidebar");
-    sideNav.classList.toggle("show");
-    menuBtn.children[0].classList.toggle("fa-times")
-})
+menuBtn.addEventListener("click", () => {
+  const sideNav = document.querySelector(".sidebar");
+  sideNav.classList.toggle("show");
+  menuBtn.children[0].classList.toggle("fa-times");
+});
